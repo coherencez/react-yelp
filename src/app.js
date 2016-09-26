37e1178acdefa2fdfddab04b7e9e459ae01ccd3e
Thomas Swann <tkswann@mac.com>
2016-09-25 21:03:48 -0500

    pass browserHistory to App

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 
 import './app.css'
 
+import {browserHistory} from 'react-router'
+
 import App from 'containers/App/App'
 
 
 const mountNode = document.querySelector('#root')
-ReactDOM.render(<App />, mountNode)
+ReactDOM.render(<App history={browserHistory} />, mountNode)

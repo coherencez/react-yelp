65bdefd01214b73b2b6ac563b9ea0267c3718cec
Thomas Swann <tkswann@mac.com>
2016-09-25 21:21:27 -0500

    actual routing in place and being passed to App

--- src/app.js
+++ src/app.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'font-awesome/css/font-awesome.css'
 import './app.css'
-
-import { browserHistory, Router, Route } from 'react-router'
 
 import App from 'containers/App/App'
 
-const Home = React.createClass({
-  render: function() {
-    return (<div>Hello World</div>)
-  }
-})
-const routes = (
-  <Router>
-    <Route path='/' component={Home} />
-  </Router>
-)
+import { browserHistory } from 'react-router'
+import makeRoutes from './routes'
+
+const routes = makeRoutes()
 
 const mountNode = document.querySelector('#root')
-ReactDOM.render(<App history={browserHistory} routes={routes} />, mountNode)
+ReactDOM.render(
+  <App
+    history={browserHistory}
+    routes={routes}
+  />, mountNode)

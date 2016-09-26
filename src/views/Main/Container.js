44a7306452c8c0152bd1dac65d22edb8c3b105f0
Thomas Swann <tkswann@mac.com>
2016-09-25 23:16:59 -0500

    Component module styling

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -3,6 +3,7 @@ import Map, {GoogleApiWrapper} from 'google-maps-react'
 import {searchNearby} from 'utils/googleApiHelpers'
 
 import Header from 'components/Header/Header'
+import styles from './styles.module.css'
 
 
 export class Container extends React.Component {
@@ -42,12 +43,17 @@ export class Container extends React.Component {
         <Map
           google={this.props.google}
           onReady={this.onReady.bind(this)}
-          visible={false}>
+          visible={false}
+          className={styles.wrapper}>
+
           <Header />
 
-          {this.state.places.map(place => {
-            return (<div key={place.id}>{place.name}</div>)
-          })}
+          <div className={styles.content}>
+            {this.state.places.map(place => {
+              return (<div key={place.id}>{place.name}</div>)
+            })}
+          </div>
+          
         </Map>
       </div>
     )

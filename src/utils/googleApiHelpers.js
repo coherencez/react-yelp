beb6383d4ad0efb256d58fcfa917db402ea9871d
Thomas Swann <tkswann@mac.com>
2016-09-25 22:21:10 -0500

    searchNearby helper function created

---
+++ src/utils/googleApiHelpers.js
@@ -0,0 +1,13 @@
+export const searchNearby = (google, map, request) => {
+  return new Promise((resolve, reject) => {
+    const service = new gogle.maps.places.PlacesService(map)
+
+    service.nearbySearch(request, (results, status, pagination) => {
+      if(status == google.maps.places.PlacesServiceStatus.OK) {
+        resolve(results, pagination)
+      } else {
+        reject(results, status)
+      }
+    })
+  })
+}
--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
 
 
-class Container extends React.Component {
+export class Container extends React.Component {
+  onReady() {
+    // when map is ready and mounted
+  }
   render() {
     return (
       <div>
         Hello from the container
         <Map
+          onReady={this.onReady.bind(this)}
           google={this.props.google}
         />
       </div>

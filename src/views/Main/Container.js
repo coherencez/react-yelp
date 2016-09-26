200a8b1391fb7489a2b64220a0309d3b549a73a2
Thomas Swann <tkswann@mac.com>
2016-09-25 22:27:29 -0500

    implemente promise in Container for Map

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
+import {searchNearby} from 'utils/googleApiHelpers'
 
 
 export class Container extends React.Component {
-  onReady() {
+  onReady(mapProps, map) {
     // when map is ready and mounted
+    const {google} = this.props
+    const opts = {
+      location: map.center,
+      radius: '500',
+      types: ['cafe']
+    }
+    searchNearby(google, map, opts)
+      .then((results, pagination) => {
+        // we got some result and a pagination obj
+      })
+      .catch((status, result) => {
+        // there was an error
+      })
   }
   render() {
     return (

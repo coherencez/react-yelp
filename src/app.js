b1b0db46e05450a8b9b2bf85f45539aa86bc7a29
Thomas Swann <tkswann@mac.com>
2016-09-25 19:03:08 -0500

    initial boiplerplate and webpack setup for yelp tut

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Text here
+      </div>
+    )
+  }
+})
+
+const mountNode = document.querySelector('#root')
+ReactDOM.render(<App />, mountNode)

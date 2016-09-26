2db93d547f27518cf88cf8d5751838069acc7af7
Thomas Swann <tkswann@mac.com>
2016-09-25 20:07:41 -0500

    setup webpack to track env vars for prod/dev - api keys etc -- added font-awesome

--- webpack.config.js
+++ webpack.config.js
@@ -66,4 +66,18 @@ const environmentEnv = dotenv.config({
 const envVariables =
   Object.assign({}, dotEnvVars, environmentEnv)
 
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo,key) => {
+    const val = JSON.stringify(envVariables[key])
+    memo[`__${key.toUpperCase()}__`] = val
+    return memo
+  },{
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  })
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins)
+
 module.exports = config
